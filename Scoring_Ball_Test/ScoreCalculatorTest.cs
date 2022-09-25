7ebbf5f19ca61c8b4aed5216aee06940c710dcb6
Gourav Tiwari <gotiwari@affinionds.com>
2022-09-25 11:15:25 +0530

    Rename directory

---
+++ Scoring_Ball_Test/ScoreCalculatorTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scoring_ball_1;
+
+namespace Scoring_Ball_Test
+{
+    public  class ScoreCalculatorTest
+    {
+        ScoreCalculator scoreCalculator = new ScoreCalculator();
+       
+        [Fact]
+        public void Calculate_IfStrike_And_PrevSpareIsTrue()
+        {
+            ScoreCard scoreCard = new ScoreCard();
+            int[] score = new int[] { 10, 0 };
+            scoreCard.sparehandler.AssignedSpare();
+            int ActualResult=scoreCalculator.Calculate(score, scoreCard);
+            int ExpectedResult = 20;
+            Assert.Equal(ExpectedResult, ActualResult);
+
+        }
+        public void Calculate_IfStrike_And_StrikeCountIsThree()
+        {
+            ScoreCard scoreCard = new ScoreCard();
+            int[] score = new int[] { 10, 0 };
+            scoreCard.strikehandler.IncreaseStrikeCount();
+            scoreCard.strikehandler.IncreaseStrikeCount();
+            scoreCard.strikehandler.IncreaseStrikeCount();
+            int ActualResult = scoreCalculator.Calculate(score, scoreCard);
+            int ExpectedResult = 30;
+            Assert.Equal(ExpectedResult, ActualResult);
+
+        }
+        [Fact]
+        public void Calculate_IfNotStrike_StrikeCountIsOne()
+        {
+            ScoreCard scoreCard = new ScoreCard();
+            int[] score = new int[] { 5, 5 };
+            scoreCard.strikehandler.IncreaseStrikeCount();
+            int actualResult=scoreCalculator.Calculate(score, scoreCard);
+            int expectedResult = 10 + score[0] + score[1];
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+
+        [Fact]
+        public void Calculate_IfNotStrike_StrikeCountIsTwo()
+        {
+            ScoreCard scoreCard = new ScoreCard();
+            int[] score = new int[] { 5, 5 };
+            scoreCard.strikehandler.IncreaseStrikeCount();
+            scoreCard.strikehandler.IncreaseStrikeCount();
+            int actualResult = scoreCalculator.Calculate(score, scoreCard);
+            int expectedResult = 30 + 2*score[0] + score[1];
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+
+        [Fact]
+        public void Calculate_IfNotStrike_IsPrevSpare()
+        {
+            ScoreCard scoreCard = new ScoreCard();
+            int[] score = new int[] { 5, 5 };
+            scoreCard.sparehandler.AssignedSpare();
+            int actualResult = scoreCalculator.Calculate(score, scoreCard);
+            int expectedResult = 10 + score[0];
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+
+    }
+}
---
+++ Scoring_Ball_Test/ScoreRoundTellerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scoring_ball_1;
+
+namespace Scoring_Ball_Test
+{
+    public  class ScoreRoundTellerTest
+    {
+        //ScoreCard scoreCard = new ScoreCard();
+        ScoreCard scoreCard=new ScoreCard();
+
+        [Fact]
+        public void GetRound_ReturnRound()
+        {
+            ScoreRoundTeller scoreRoundTeller = new ScoreRoundTeller(scoreCard);
+            int result=scoreRoundTeller.GetRound();
+            Assert.Equal(0, result);
+
+        }
+
+        [Fact]
+        public void GetRoundScore()
+        {
+            ScoreRoundTeller scoreRoundTeller = new ScoreRoundTeller(scoreCard);
+         
+            int[] score=scoreRoundTeller.GetRoundScore(scoreCard);
+            int[] expected_core = new int[] { 3,4};
+            Assert.Equal(expected_core, score);
+
+
+        }
+
+
+    }
+}
